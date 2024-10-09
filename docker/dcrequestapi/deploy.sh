bfd1d5ad0b41c6fcb98d085a32cee3f0d6604909
Björn Quast <b.quast@leibniz-lib.de>
2024-10-09 22:39:50 +0200

    env-file does not work in swarm, so env-variables must be set by deploy.sh

--- docker/dcrequestapi/deploy.sh
+++ docker/dcrequestapi/deploy.sh
@@ -2,6 +2,7 @@ cd dcrequestapi;
 docker build --no-cache -t docker.leibniz-lib.de:5000/dcrequestapi:latest .; docker push docker.leibniz-lib.de:5000/dcrequestapi:latest;
 cd ..;
 docker stack rm dcrequestapi;
+export $(cat compose.env);
 docker stack deploy --with-registry-auth -c docker-compose-swarm.yml dcrequestapi;
 
 
